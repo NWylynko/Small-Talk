c53555bd917358afbc79d210ab49ef4608666126
Nicholas Wylynko <1014375@student.ccgs.wa.edu.au>
2019-10-21 20:33:59 +0800

    add images

---
+++ screens/main/images.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, View, TouchableOpacity } from "react-native";
+
+export default function Images({  }) {
+  return (
+    <TouchableOpacity>
+
+    </TouchableOpacity>
+  );
+}
